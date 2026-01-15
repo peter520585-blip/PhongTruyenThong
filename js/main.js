ccf483288c214069fed74375a61c0505060fa6a5
peter520585-blip <peter520585@gmail.com>
2026-01-15 07:58:27 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const model = document.createElement('a-entity');
       model.setAttribute('gltf-model', `url(${item.modelSrc})`);
-      model.setAttribute('rotation', '0 0 180');
+      model.setAttribute('rotation', '0 90 0');
       model.setAttribute('transparent-model', 'opacity: 0.8');
       
       modelContainer.appendChild(model);
@@ -151,6 +151,7 @@ document.addEventListener("DOMContentLoaded", () => {
     btnExit.style.display = 'none';
   });
 });
+
 
 
 
